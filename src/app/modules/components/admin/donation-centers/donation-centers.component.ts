e6b0d1fc724ced9917c90e18e4adfe6a66520960
AdrianMoldovan97 <moldovanadrian19@gmail.com>
2019-03-24 17:50:48 +0200

    Implement feature for add and remove a donation center

--- src/app/modules/components/admin/donation-centers/donation-centers.component.ts
+++ src/app/modules/components/admin/donation-centers/donation-centers.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { DonationCenterService } from 'src/core/donation-center-service/donation-center.service';
 import { RealLocation } from 'src/shared/models/locations/real-location';
+import { EnumsService } from 'src/core/enums-service/enums-service';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { LocationResponse } from 'src/shared/models/locations/location-response';
+import { BooleanServerResponse } from 'src/shared/models/boolean-server-response/boolean-server-response';
 
 @Component({
   selector: 'app-donation-centers',
@@ -9,16 +13,61 @@ import { RealLocation } from 'src/shared/models/locations/real-location';
 })
 export class DonationCentersComponent implements OnInit {
 
-    constructor(private donationCenterService: DonationCenterService) { }
+    constructor(private donationCenterService: DonationCenterService, private enumService : EnumsService) { }
 
     donationCenters: RealLocation[];
+    donationCenterForm: FormGroup;
+    counties: string[];
 
     ngOnInit() {
+      const formBuilder = new FormBuilder();
+      
         this.donationCenterService.getDonationCenters().subscribe(
             (res: RealLocation[]) => {
               this.donationCenters = res;
             }
         );
+
+        this.enumService.getEnums().subscribe(res => {
+          this.counties = res.object.counties;
+        });
+
+        this.donationCenterForm = formBuilder.group({
+          name: ['', Validators.required],
+          address: ['', Validators.required],
+          latitude: ['', Validators.required],
+          longitude: ['', Validators.required],
+          county: ['', Validators.required]
+        });
     }
+
+    enumToOption(enu: string): string {
+      return enu.split('_')
+     .map((s: string) => s.slice(0, 1) + s.slice(1).toLowerCase())
+     .join(' ');
+ }
+
+ addDonationCenter() {
+   const donationCenter = this.donationCenterForm.value;
+   this.donationCenterService.addDonationCenter(donationCenter).subscribe(
+     (res: LocationResponse) => {
+       if(res.success){
+         this.donationCenters.push(res.model);
+       }else{
+         alert(res.exception)
+       } 
+     } );
+   }
+
+   removeDonationCenter(centerId: number, index : number) {
+     this.donationCenterService.removeDonationCenter(centerId)
+       .subscribe((res: BooleanServerResponse) => {
+         if (res.success === true){
+           this.donationCenters.splice(index, 1);
+         }else{
+           alert(res.exception)
+         }
+       });
+   }
 
 }
